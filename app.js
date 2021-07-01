bdb175d9d6dcf988dc5fe9f1964980684cd6dadf
Triana Cerda <trianaRcerda@gmail.com>
2021-07-01 14:30:07 -0700

    imagies up and rules

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // import functions and grab DOM elements
-
+import { didUserWin } from "./utils.js";
 // initialize state
-
+const playBtn = document.getElementById('btn');
+con 
 // set event listeners 
   // get user input
   // use user input to update state 
---
+++ utils.js
@@ -0,0 +1,23 @@
+export function didUserWin(userThrow, compThrow){
+    if (userThrow === compThrow)
+        return 'draw'
+
+    if else (userThrow )    
+
+}
+
+// userThrow
+// rock, paper, scissors
+
+// computerThrow
+// rock, paper, scissors
+
+// if the userThrow is the same as the computerThrow
+//   return 'draw'
+// if the user throws rock
+//    if computerThrow is scissors return 'win'
+// if the user throws paper
+//    if computerThrow is rock return 'win'
+// if the user throw scissors
+//    if computerThrow is paper return 'win'
+// return 'lose'
